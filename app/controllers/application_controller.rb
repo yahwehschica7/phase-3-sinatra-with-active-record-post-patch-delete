6059242face333d10d757cdf297f3d0b65fa9db1
Jessica Nelson <yahwehschica@yahoo.com>
2022-05-17 12:42:07 -0400

    commit tree 5f9b1ff76aa322410dbf2839e881ef3edb2ecd56

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  post '/reviews' do 
+    review = Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+  )
+  review.to_json 
+  end
+
+  patch '/reviews/:id' do 
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -15,5 +36,13 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+ delete '/reviews/:id' do
+  review = Review.find(params[:id])
+  review.destroy
+  review.to_json
+ end
+
+ 
 
 end
